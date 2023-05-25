c34a1eca5d3ba3e00d69b7d31b63ba14db703654
Jesus Molina <netod90@gmail.com>
2023-05-25 15:29:29 -0700

    Fix linter

--- src/__tests__/CoinsDetail.test.js
+++ src/__tests__/CoinsDetail.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../app/store';
-import CoinsDetail from '../routes/coinsDetail';
+import CoinsDetail from '../routes/CoinsDetail';
 
 describe('Test CoinsDetail page', () => {
   it('Renders correctly', () => {
--- src/__tests__/HomePage.test.js
+++ src/__tests__/HomePage.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../app/store';
-import HomePage from '../routes/homePage';
+import HomePage from '../routes/HomePage';
 
 describe('Test HomePage', () => {
   it('Renders correctly', () => {
